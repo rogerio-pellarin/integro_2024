2976023134dcd93fb435629c4519e433077699bf
Brandon Alexis <brandonmachado159@gmail.com>
2024-03-11 18:07:31 -0500

    adding slice for boxes with 4 apples

--- test2.js
+++ test2.js
@@ -1,7 +1,14 @@
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 function apples(input) {
+  let result = [];
+  let slice_quantity = 4;
 
+  for (let i = 0; i < input.length; i += slice_quantity) {
+    result.push(input.slice(i, i + slice_quantity));
+  }
+
+  return result;
 }
 
-console.log(apples(input))
+console.log(apples(input));
