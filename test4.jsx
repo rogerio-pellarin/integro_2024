6126c312e5a72eaad38db7700e11332fa992807f
José Cuevas <jcuevasinfante17@gmail.com>
2024-03-11 13:54:48 -0500

    test react

--- test4.jsx
+++ test4.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
-import axios from 'axios';
+// export default class Palindrome extends React.Component {
 
-export default class Palindrome extends React.Component {
+//     render() {
+//         return (
+//             <>
+//             </>
+//         )
+//     }
+// }
 
-    render() {
-        return (
-            <>
-            </>
-        )
+import React, { useState } from "react";
+import axios from "axios";
+
+function PalindromeChecker() {
+  const [inputText, setInputText] = useState("");
+  const [isPalindrome, setIsPalindrome] = useState(null);
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const checkPalindrome = async () => {
+    try {
+      const response = await axios.post("/api/checkPalindrome", {
+        text: inputText,
+      });
+      setIsPalindrome(response.data.isPalindrome);
+    } catch (error) {
+      console.error("Error checking palindrome:", error);
     }
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputText} onChange={handleInputChange} />
+      <button onClick={checkPalindrome}>Check Palindrome</button>
+      {isPalindrome !== null && (
+        <p>
+          {isPalindrome ? "It is a palindrome!" : "It is not a palindrome!"}
+        </p>
+      )}
+    </div>
+  );
 }
+
+export default PalindromeChecker;
