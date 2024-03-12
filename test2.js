d66a5acbf5b41f0da31e4700719c69729873bcc6
lisbeth <lmaciascantillo@gmail.com>
2024-03-12 16:56:35 -0500

    test2 and test3 completed

--- test2.js
+++ test2.js
@@ -1,7 +1,20 @@
 var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
-function apples(input) {
+function apples(input) { // there is a basket with a variable number of apples (e.g., 17)
+    const appleBox = 4; // each box containing a fixed quantity (e.g., 4 apples)
+    const packsBox = 2; // with each pack consisting of two boxes
+    const packsFinish = []; 
 
+    while (input.length > 0) {
+        const boxes = [];
+        for (let i = 0; i < packsBox; i++) {
+            const box = input.splice(0, appleBox);
+            boxes.push(box);
+        }
+        packsFinish.push(boxes);
+    }
+    return packsFinish;
 }
-
 console.log(apples(input))
+
+
---
+++ test3.sql
@@ -0,0 +1,6 @@
+SELECT u.name AS user_name, u.birthdate, c.name AS customer_name
+FROM user AS u
+JOIN user_customer AS uc ON u.id = uc.user_id
+JOIN customer AS c ON uc.customer_id = c.id
+WHERE c.status = 1 AND u.status = 1 AND u.birthdate <= DATE_SUB(CURDATE(), INTERVAL 18 YEAR) 
+ORDER BY c.name;
