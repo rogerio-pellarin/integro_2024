8d484a717d8c35d179f71bc9f5a903c2c7cb8bb4
danielNieto <danielloco471@gmail.com>
2024-03-12 13:19:42 -0500

    Se realizan algunos ajustes

--- test1.php
+++ test1.php
@@ -1,11 +1,20 @@
 <?php
 
 function is_palindrome($input) {
+    $input = strtolower(preg_replace("/[^a-z0-9áéíóúñ\s]+/i", "", $input));
+
+
     $words = explode(' ', $input);
-    $rever_words = array_reverse($words);
-    $reversed_expression = implode(' ', $rever_words);
-    return $input === $rev_expression;
+
+
+    $reversed_words = array_reverse($words);
+
+
+    $reversed_expression = implode(' ', $reversed_words);
+
+    
+    return $input === $reversed_expression;
 }
 
-$input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
+$input = "ablewasIereIsawelba";
 var_dump(is_palindrome($input));
