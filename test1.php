77cf9ad8651b4af830b8393e4f9ab92433607172
vettotriana <vetto@hotmail.co.uk>
2024-03-13 15:04:06 -0500

    Update test1.php

--- test1.php
+++ test1.php
@@ -1,8 +1,12 @@
 <?php
 
 function is_palindrome($input) {
-    //solve this method that says if the sentence is palindrome or not
+    // Remove non-alphanumeric characters and convert to lowercase
+    $normalized = strtolower(preg_replace('/[^A-Za-z0-9]/', '', $input));
+    // Check if the normalized string is equal to its reverse
+    return $normalized === strrev($normalized);
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
 var_dump(is_palindrome($input));
+?>
