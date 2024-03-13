7152259c37de6e1dfa45c89fd5d9b00763451071
vettotriana <vetto@hotmail.co.uk>
2024-03-13 15:05:34 -0500

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,7 +1,29 @@
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+function organizeApples(apples, boxSize = 4, packsPerFriend = 2) {
+    // Function to chunk an array into smaller arrays of a specified size
+    function chunkArray(array, size) {
+        const chunkedArr = [];
+        for (let i = 0; i < array.length; i += size) {
+            chunkedArr.push(array.slice(i, i + size));
+        }
+        return chunkedArr;
+    }
 
-function apples(input) {
+    // Step 1: Divide apples into boxes
+    const boxes = chunkArray(apples, boxSize);
 
+    // Step 2: Group boxes into packs
+    const packs = chunkArray(boxes, 2); // Assuming a pack consists of 2 boxes
+
+    // Step 3: Distribute packs among friends
+    const friends = [];
+    for (let i = 0; i < packs.length; i += packsPerFriend) {
+        friends.push(packs.slice(i, i + packsPerFriend));
+    }
+
+    return friends;
 }
 
-console.log(apples(input))
+// Example usage
+const apples = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+const organizedApples = organizeApples(apples);
+console.log(JSON.stringify(organizedApples, null, 2));
