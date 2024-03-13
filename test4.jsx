d16f682a2b983a6507f6f523551aad545a17f2d6
vettotriana <vetto@hotmail.co.uk>
2024-03-13 15:07:44 -0500

    Update test4.jsx

--- test4.jsx
+++ test4.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import axios from 'axios';
 
 export default class Palindrome extends React.Component {
+    state = {
+        text: '',
+        isPalindrome: null
+    };
+
+    checkPalindrome = async () => {
+        try {
+            const response = await axios.post('/api/check-palindrome', { text: this.state.text });
+            // Assuming the API returns a JSON object with a boolean property "isPalindrome"
+            this.setState({ isPalindrome: response.data.isPalindrome });
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+    };
+
+    handleTextChange = (event) => {
+        this.setState({ text: event.target.value });
+    };
 
     render() {
         return (
             <>
+                <input type="text" value={this.state.text} onChange={this.handleTextChange} placeholder="Enter text" />
+                <button onClick={this.checkPalindrome}>Check Palindrome</button>
+                {this.state.isPalindrome !== null && (
+                    <p>{this.state.text} is {this.state.isPalindrome ? '' : 'not'} a palindrome.</p>
+                )}
             </>
-        )
+        );
     }
 }
