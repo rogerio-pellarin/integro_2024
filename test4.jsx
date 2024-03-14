9b2e1d79c01e2ec32a94836d49bfd3a9d798a58d
maserna06 <maserna06@gmail.com>
2024-03-14 00:12:26 -0500

    Solution TEST 4 70%

--- test4.jsx
+++ test4.jsx
@@ -3,9 +3,20 @@ import axios from 'axios';
 
 export default class Palindrome extends React.Component {
 
+    getData = async () => {
+        const resPal = await axios.post( 'http://localhost:8080/jobs-tests/integro_2024/test1.php', {
+            frase: frase,
+        });
+
+        console.log('resPal:', resPal);
+    }
+
     render() {
         return (
             <>
+                <input type="text" name="frase" id="" />
+                <br />
+                <input type="button" value="" onClick={ this.getData }/>
             </>
         )
     }
